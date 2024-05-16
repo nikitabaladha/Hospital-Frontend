c936a6161e4b0998f8213b43c60672eeab850905
nikitabaladha <nikitabaladha1996@gmail.com>
2024-05-16 16:24:27 +0530

    Installed bootstrap and use it in navbar component

--- hospital-forntend/src/components/Navbar/Navbar.js
+++ hospital-forntend/src/components/Navbar/Navbar.js
@@ -1,37 +1,70 @@
-// components/Navbar/Navbar.js
+// components / Navbar / Navbar.js;
+
 import React from "react";
 import { Link } from "react-router-dom";
+import "./index.css";
+import logoImage from "../../images/aayushakti.png";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/service">Service</Link>
-        </li>
-        <li>
-          <Link to="/doctors">Doctors</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/appointment">Make an Appointment</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img id="brand-logo" src={logoImage} alt="Logo" />
+        </Link>
+
+        <div className="justify-content-center" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/service">
+                Service
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/doctors">
+                Doctors
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/blog">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="btn btn-primary" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="btn btn-success" to="/appointment">
+                Make an Appointment
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
+
 export default Navbar;
